c49777c4c31a027f1fce947e2135ad344e2c2f1d
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-01-05 13:33:22 +0100

    file_line with match

--- 01-resources/stdlib/file_line.pp
+++ 01-resources/stdlib/file_line.pp
@@ -13,3 +13,11 @@ file_line { 'Append a line to /tmp/first.txt':
     line    => "\n\nYet another line...\n\n",
     require => File['/tmp/first.txt']
 }
+
+
+file_line { 'Append a line to /tmp/first.txt if it does not exist':
+    path    => '/tmp/first.txt',
+    line    => "\nthis is a line\n",
+    match   => "\nthis is a line\n",
+    require => File['/tmp/first.txt']
+}
