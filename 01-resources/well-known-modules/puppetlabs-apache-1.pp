2bc1cdfce3316890d7648092b5b482f26f920b86
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-01-05 13:24:44 +0100

    Apache: single virtual host

---
+++ 01-resources/well-known-modules/puppetlabs-apache-1.pp
@@ -0,0 +1,33 @@
+# Ubuntu
+#   sudo puppet module install puppetlabs-apache --version 1.2.0
+#   sudo puppet apply puppetlabs-apache.pp
+#   sudo apt-get install lynx-cur -y
+
+
+class {
+  'apache':  default_vhost => false;
+}
+
+file { [
+        '/vagrant/tmp',
+        '/vagrant/tmp/by-examples',
+    ]:
+    ensure => directory,
+}
+
+file { '/vagrant/tmp/by-examples/index.html':
+    content => '<h1>Hello, by-examples!</h1>',
+}
+
+apache::vhost { 'by-examples.lh':
+  port          => '80',
+  docroot       => '/vagrant/tmp/by-examples',
+  docroot_owner => 'vagrant',
+  docroot_group => 'vagrant',
+}
+
+file_line { 'Append by-examples.lh to /etc/hosts':
+    path  => '/etc/hosts',
+    line  => "127.0.0.1 by-examples.lh\n",
+    match => "127.0.0.1 by-examples.lh\n",
+}
