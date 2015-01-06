9d33fd95af7b0b1570018f662f12b1213e81e63a
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-01-06 10:38:13 +0100

    Wget module example

--- 01-resources/well-known-modules/wget-1.pp
+++ 01-resources/well-known-modules/wget-1.pp
@@ -1,1 +1,9 @@
-# How to use wget module
+# Ubuntu
+#   sudo puppet module install maestrodev-wget
+#   sudo puppet apply wget-1.pp
+
+wget::fetch { 'get-by-examples-net':
+    source      => 'http://by-examples.net',
+    destination => '/tmp/by-examples-index.html',
+}
+
