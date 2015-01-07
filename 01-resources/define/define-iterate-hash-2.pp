4df51205366abd77b3578e078c52ddcc251d46cf
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-01-07 07:39:40 +0100

    Iterate hash - example 2

---
+++ 01-resources/define/define-iterate-hash-2.pp
@@ -0,0 +1,32 @@
+$list_of_phars = [
+  {
+      'app' => 'composer',
+      'url' => 'https://getcomposer.org/composer.phar'
+  },
+  {
+      'app' => 'phpunit',
+      'url' => 'https://phar.phpunit.de/phpunit.phar'
+  },
+  {
+      'app' => 'php-cs-fixer',
+      'url' => 'http://cs.sensiolabs.org/get/php-cs-fixer.phar'
+  },
+  {
+      'app' => 'box',
+      'url' => 'https://github.com/box-project/box2/releases/download/2.5.0/box-2.5.0.phar'
+  },
+  {
+      'app' => 'symfony',
+      'url' => 'http://symfony.com/installer'
+  }
+]
+
+define loop (
+    $user = 'root'
+) {
+    notify { "User: ${user} // App ${name['app']} // Url ${name['url']}": }
+}
+
+loop { $list_of_phars:
+ user => 'ipsum',
+}
