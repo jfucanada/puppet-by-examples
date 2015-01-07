f97d540e332e6212fd0e3bd461452fbf2eef6d40
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-01-07 07:58:49 +0100

    Define with name validator

---
+++ 01-resources/define/define-iterate-hash-3.pp
@@ -0,0 +1,35 @@
+# validate the type of parameter passed to loop
+
+include stdlib
+
+$list = [
+  {
+      'app' => 'CAT',
+      'url' => 'DOG'
+  },
+  {
+      'app' => 'SKY',
+  },
+  {
+      'url' => 'CLOUDS',
+  }
+]
+
+define loop (
+    $user = 'root'
+) {
+    if is_hash($name) and has_key($name, 'app') and has_key($name, 'url') {
+        notify { "User: ${user} // App ${name['app']} // Url ${name['url']}": }
+    } else {
+        warning('ERROR')
+    }
+}
+
+loop { 'ABC':
+ user => 'ipsum',
+}
+
+loop { $list:
+ user => 'ipsum',
+}
+
